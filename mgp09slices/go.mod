0e946671d6d01b873102e0fa161733fbc45214d8
MartinMSalas <ingmartinmsalas@gmail.com>
2023-10-14 22:44:20 -0300

    add slices

---
+++ mgp09slices/go.mod
@@ -0,0 +1,3 @@
+module master_go_programming/mgp09slices
+
+go 1.21.2
---
+++ mgp09slices/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var cities []string
+	fmt.Printf("%#v\n", cities)
+
+	cities = []string{"Paris", "London", "Tokyo", "Catamarca", "Cordoba"}
+
+	fmt.Printf("%#v\n", cities)
+	cities[4] = "New York"
+	fmt.Printf("%#v\n", cities)
+
+	for index,value := range cities {
+		fmt.Printf("cities: %d is %s\n", index, value)
+	}
+}
