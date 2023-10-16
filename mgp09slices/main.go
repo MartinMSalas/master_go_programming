345ce28bb606c421f4ef8bd823c50a06b5aab5aa
MartinMSalas <ingmartinmsalas@gmail.com>
2023-10-15 23:28:27 -0300

    comparing slices

--- mgp09slices/main.go
+++ mgp09slices/main.go
@@ -12,7 +12,36 @@ func main() {
 	cities[4] = "New York"
 	fmt.Printf("%#v\n", cities)
 
-	for index,value := range cities {
+	for index, value := range cities {
 		fmt.Printf("cities: %d is %s\n", index, value)
 	}
+
+	var n []int
+	fmt.Println(n == nil)
+
+	m := []int{}
+	fmt.Println(m == nil)
+	fmt.Println(m)
+	a, b := []int{1, 2, 3}, []int{1, 2, 3, 4}
+	fmt.Println(len(a) + len(b))
+
+	var eq bool = true
+
+	if len(a) != len(b) {
+		eq = false
+	} else {
+
+		for i := range a {
+			if a[i] != b[i] {
+				eq = false
+				break
+			}
+		}
+	}
+	if eq {
+		fmt.Println("equal")
+	} else {
+		fmt.Println("not equal")
+	}
+
 }
